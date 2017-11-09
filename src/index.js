7ec50dce19d4050d06bae2148ae2edad896113ca
ihfazhillah <mihfazhillah@gmail.com>
2017-11-09 09:40:35 +0700

    add login static template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import App from "./containers/App";
+import Login from "./containers/Login";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <HashRouter>
+    <Switch>
+      <Route path="/messages" component={App} />
+      <Route path="/login" component={Login} />
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
