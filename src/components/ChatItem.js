0653d62ba3042e0b8c37bdf4d1c0a8b18910407b
ihfazhillah <mihfazhillah@gmail.com>
2017-11-13 06:21:38 +0700

    add default image

--- src/components/ChatItem.js
+++ src/components/ChatItem.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Media } from "reactstrap";
+import dummyprofile from "../images/dummy-profile.png";
 
 export default class ChatItem extends Component {
   render() {
     return (
       <Media style={{ padding: "0 12px" }}>
         <Media left href="#">
-          <Media object src="http://lorempixel.com/64/64" />
+          <Media object src={dummyprofile} />
         </Media>
         <Media body>
           <Media heading>Head</Media>
