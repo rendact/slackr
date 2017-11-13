6fffdf82f93cafa5a6ecd3092006999ab421ee79
ihfazhillah <mihfazhillah@gmail.com>
2017-11-13 18:43:30 +0700

    make old subscriptions-transport-ws work with apollo-link

--- src/apollo.js
+++ src/apollo.js
@@ -1,18 +1,44 @@
 import ApolloClient from "apollo-client";
+import { split, ApolloLink } from "apollo-link";
 import { createHttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+
+class CustomWebSocketLink extends ApolloLink {
+  constructor(client) {
+    super();
+    this.client = client;
+  }
+
+  request(operation, forward) {
+    return this.client.request(operation);
+  }
+}
+
+const token = localStorage.getItem("slackrToken");
+
+const wsClient = new SubscriptionClient(
+  "wss://us-west-2.api.scaphold.io/graphql/slack-slack-me",
+  {
+    reconnect: true,
+    timeout: 2000
+  }
+);
+
+const wsLink = new CustomWebSocketLink(wsClient);
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GQLURI
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("slackrToken");
-
   // got error here, when no token provided. always return 401 unauthorized error.
   // so, i make checking. if there token, this will use authorization header. otherwise
   // use default headers
+  const token = localStorage.getItem("slackrToken");
 
   if (token)
     return {
@@ -25,7 +51,18 @@ const authLink = setContext((_, { headers }) => {
   return { headers: { ...headers } };
 });
 
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
 export default new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: link,
   cache: new InMemoryCache()
 });
