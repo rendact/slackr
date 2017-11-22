59d615887aedc63a804816256144614936005eb5
ihfazhillah <mihfazhillah@gmail.com>
2017-11-22 11:11:30 +0700

    add gear in header

--- src/components/MessagesHead.js
+++ src/components/MessagesHead.js
@@ -10,6 +10,10 @@ export default class MessagesHead extends Component {
             className={type === "private" ? "fa fa-lock" : "fa fa-hashtag"}
           />{" "}
           {name}
+          <span
+            className="fa fa-cog"
+            style={{ float: "right", color: "white", paddingRight: 10 }}
+          />
         </h2>
         <hr />
       </div>
