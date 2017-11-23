92880c0be0ce53ac918a620653d3ac01625739c8
ihfazhillah <mihfazhillah@gmail.com>
2017-11-23 10:42:44 +0700

    add chevron in profile

--- src/scenes/Messages/components/Sidebar/components/SidebarHead/index.js
+++ src/scenes/Messages/components/Sidebar/components/SidebarHead/index.js
@@ -4,8 +4,14 @@ export default class SidebarHead extends Component {
   render() {
     return (
       <div id="sidebar-head" onClick={this.props.onClick}>
-        <h2>{this.props.title}</h2>
-        <p className="text-muted">{this.props.tagline}</p>
+        <div>
+          <h2>{this.props.title}</h2>
+          <p className="text-muted">{this.props.tagline}</p>
+        </div>
+        <span
+          className="fa fa-chevron-down"
+          style={{ float: "right", transform: "translate(0, -55px)" }}
+        />
         <hr />
       </div>
     );
