550c20639d9e9aadbffdc98da864dab7cb17d6fd
ihfazhillah <mihfazhillah@gmail.com>
2017-11-23 17:54:28 +0700

    initial form look like

--- src/scenes/Messages/components/Sidebar/components/AccountSettingModal/components/Modal/index.js
+++ src/scenes/Messages/components/Sidebar/components/AccountSettingModal/components/Modal/index.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
-import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  FormText,
+  Media,
+  Button
+} from "reactstrap";
+import image from "images/dummy-profile.png";
 
 class AccountModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onImageClick = this.onImageClick.bind(this);
+  }
+  onImageClick(e) {
+    debugger;
+  }
   render() {
     return (
-      <Modal isOpen={true}>
+      <Modal isOpen={true} size="lg">
         <ModalHeader>Account Name Setting</ModalHeader>
-        <ModalBody>This is the form</ModalBody>
+        <ModalBody>
+          <div>
+            <Label>Profile Picture</Label>
+            <Media
+              onClick={this.onImageClick}
+              object
+              src={image}
+              className="img-thumbnail"
+              style={{ display: "block", width: 100, cursor: "pointer" }}
+            />
+            <input
+              ref={input => {
+                this.file = input;
+              }}
+              type="file"
+              style={{ visibility: "hidden", height: 0, width: 0 }}
+            />
+          </div>
+          <FormGroup>
+            <Label>Full Name</Label>
+            <Input type="text" placeholder="Your Full Name" />
+          </FormGroup>
+          <FormGroup>
+            <Label>Display Name</Label>
+            <Input type="text" placeholder="The display name" />
+            <FormText>aka short name</FormText>
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input type="password" placeholder="enter a new password" />
+            <FormText>aka short name</FormText>
+          </FormGroup>
+        </ModalBody>
         <ModalFooter>
-          <small className="text-muted">upSlack</small>
+          <Button color="primary">Submit</Button>
         </ModalFooter>
       </Modal>
     );
