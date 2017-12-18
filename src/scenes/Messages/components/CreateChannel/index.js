9b2c14e2a1c0e38079723ec54ba1cd14351c4be8
ihfazhillah <mihfazhillah@gmail.com>
2017-12-18 14:58:27 +0700

    reset create channel mutation

--- src/scenes/Messages/components/CreateChannel/index.js
+++ src/scenes/Messages/components/CreateChannel/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { graphql, withApollo, compose } from "react-apollo";
+import { graphql } from "react-apollo";
 
 import CreateChannelModal from "./components/CreateChannelModal";
 import { createChannel } from "scenes/Messages/components/Sidebar/queries/createChannel";
 import { bindUserChannel } from "./queries/bindUserChannel";
-import bindUserRole from "./queries/bindUserRole";
-import getAdminChannel from "./queries/getAdminChannel";
 import { createChannelToggle } from "scenes/Messages/actions/createChannel";
 import { toggleCreateChannelSubmit } from "./actions/toggleCreateChannelSubmit";
 
@@ -19,91 +17,37 @@ class CreateChannel extends Component {
 
   createChannelSubmit(val) {
     this.props.dispatch(toggleCreateChannelSubmit());
-
     return new Promise((resolve, reject) => {
-      const name = val.name + ";" + localStorage.getItem("slackrUserId");
       this.props
         .createChannel({
           variables: {
             input: {
-              name: name,
+              name: val.name,
               type: val.type
             }
           }
         })
         .then(data => {
-          this.props.dispatch(toggleCreateChannelSubmit());
-          /*
           this.props.dispatch(createChannelToggle());
 
-          const processUserChannelBinding = () =>
-            new Promise((resolve, reject) => {
-              this.props
-                .bindUserChannel({
-                  variables: {
-                    input: {
-                      channelId: data.data.createChannel.changedChannel.id,
-                      userId: localStorage.getItem("slackrUserId"),
-                      memberType: "owner"
-                    }
-                  }
-                })
-                .then(data => {
-                  resolve(data);
-                })
-                .catch(error => {
-                  reject(error);
-                });
-            });
-
-          const processUserRoleBinding = () =>
-            new Promise((resolve, reject) => {
-              this.props.client
-                .query({
-                  query: getAdminChannel
-                })
-                .then(data => {
-                  const role = data.data.viewer.allRoles.edges[0];
-
-                  if (role) {
-                    this.props
-                      .bindUserRole({
-                        variables: {
-                          roleId: role.node.id,
-                          userId: localStorage.getItem("slackrUserId")
-                        }
-                      })
-                      .then(data => {
-                        resolve(data);
-                      })
-                      .catch(error => {
-                        reject(error);
-                      });
-                  } else {
-                    throw "no adminChannel role found";
-                  }
-                });
-            });
-
-          // process all promises
-          let promise = [
-            processUserRoleBinding,
-            processUserChannelBinding
-          ].reduce((prev, task) => {
-            return prev.then(() => task()).catch(error => {
-              debugger;
-            });
-          }, Promise.resolve());
-          promise
-            .then(() => {
+          this.props
+            .bindUserChannel({
+              variables: {
+                input: {
+                  channelId: data.data.createChannel.changedChannel.id,
+                  userId: localStorage.getItem("slackrUserId"),
+                  memberType: "owner"
+                }
+              }
+            })
+            .then(data => {
+              resolve(data);
               this.props.dispatch(toggleCreateChannelSubmit());
             })
             .catch(error => {
-              debugger;
-
+              reject(error);
               this.props.dispatch(toggleCreateChannelSubmit());
             });
-        */
         })
         .catch(error => {
           reject(error);
@@ -126,10 +70,10 @@ const mapStateToProps = state => {
   return state.createChannel || {};
 };
 
-export default compose(
-  connect(mapStateToProps),
-  graphql(bindUserChannel, { name: "bindUserChannel" }),
-  graphql(createChannel, { name: "createChannel" }),
-  graphql(bindUserRole, { name: "bindUserRole" }),
-  withApollo
-)(CreateChannel);
+CreateChannel = connect(mapStateToProps)(CreateChannel);
+
+const withBinding = graphql(bindUserChannel, { name: "bindUserChannel" })(
+  CreateChannel
+);
+
+export default graphql(createChannel, { name: "createChannel" })(withBinding);
