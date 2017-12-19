94615b33357640f34381f0fc929d1d941b970c9a
ihfazhillah <mihfazhillah@gmail.com>
2017-12-19 21:31:37 +0700

    move imageinput compnent to different component

---
+++ src/scenes/Messages/components/MainMessages/components/ChatInput/components/ImageInput/index.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { graphql } from "react-apollo";
+import ImageInputModal from "./components/ImageInputModal";
+import { InputGroupAddon } from "reactstrap";
+import { createMessageMtn } from "../../queries/Message/create";
+
+class ImageInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onImageAddonClick = this.onImageAddonClick.bind(this);
+    this.imageInputModalToggle = this.imageInputModalToggle.bind(this);
+    this.onImageInputChange = this.onImageInputChange.bind(this);
+    this.onImageInputSubmit = this.onImageInputSubmit.bind(this);
+
+    this.state = {
+      isImageInputModalOpen: false
+    };
+  }
+  imageInputModalToggle(e) {
+    if (e) {
+      e.preventDefault();
+    }
+    this.setState(prevState => ({
+      isImageInputModalOpen: !prevState.isImageInputModalOpen
+    }));
+  }
+
+  onImageInputSubmit(val) {
+    return new Promise((resolve, reject) => {
+      this.props
+        .createMessage({
+          variables: {
+            input: {
+              authorId: localStorage.getItem("slackrUserId"),
+              channelId: this.props.channelId,
+              content: val.caption,
+              attachment: {
+                name: val.title,
+                blobFieldName: "image",
+                image: this.state.imageFile
+              }
+            }
+          }
+        })
+        .then(data => {
+          resolve(data);
+
+          this.imageInputModalToggle();
+        })
+        .catch(error => {
+          this.imageInputModalToggle();
+          reject(error);
+        });
+    });
+  }
+
+  onImageInputChange(e) {
+    const file = e.currentTarget.files[0];
+    if (!file) return;
+    if (!file.type.match(/image\/.*/)) {
+      throw new Error("not image error");
+    }
+
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = e => {
+      this.setState(prevState => ({
+        isImageInputModalOpen: !prevState.isImageInputModalOpen,
+        initialValues: {
+          title: file.name
+        },
+        imageFile: file,
+        imageUrl: e.target.result
+      }));
+    };
+  }
+
+  onImageAddonClick(e) {
+    this.input.click();
+  }
+  render() {
+    return (
+      <InputGroupAddon onClick={this.onImageAddonClick}>
+        <span className="fa fa-file-image-o" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          ref={input => (this.input = input)}
+          onChange={this.onImageInputChange}
+        />
+        <ImageInputModal
+          isOpen={this.state.isImageInputModalOpen}
+          toggle={this.imageInputModalToggle}
+          imageUrl={this.state.imageUrl}
+          initialValues={this.state.initialValues}
+          onSubmit={this.onImageInputSubmit}
+          onCancel={this.imageInputModalToggle}
+        />
+      </InputGroupAddon>
+    );
+  }
+}
+
+export default graphql(createMessageMtn, { name: "createMessage" })(ImageInput);
--- src/scenes/Messages/components/MainMessages/components/ChatInput/components/MessageInput/index.js
+++ src/scenes/Messages/components/MainMessages/components/ChatInput/components/MessageInput/index.js
@@ -1,35 +1,18 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
-import { graphql, compose } from "react-apollo";
-import {
-  Form,
-  InputGroup,
-  InputGroupButton,
-  InputGroupAddon,
-  FormText
-} from "reactstrap";
-import ImageInputModal from "../ImageInputModal";
-import { createMessageMtn } from "../../queries/Message/create";
+import { Form, InputGroup, InputGroupButton, FormText } from "reactstrap";
+import ImageInput from "../ImageInput";
 
 class MessageInput extends Component {
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
-    this.onImageAddonClick = this.onImageAddonClick.bind(this);
-    this.imageInputModalToggle = this.imageInputModalToggle.bind(this);
-    this.onImageInputChange = this.onImageInputChange.bind(this);
-    this.onImageInputSubmit = this.onImageInputSubmit.bind(this);
-
-    this.state = {
-      isImageInputModalOpen: false
-    };
   }
   onSubmit(val) {
     this.props
       .onSubmit(val)
       .then(() => {
         this.props.reset();
-        // by triggering this, the message body scroll to bottom
         //this.props.triggerNewMessage();
       })
       .catch(() => {
@@ -37,68 +20,6 @@ class MessageInput extends Component {
       });
   }
 
-  imageInputModalToggle(e) {
-    if (e) {
-      e.preventDefault();
-    }
-    this.setState(prevState => ({
-      isImageInputModalOpen: !prevState.isImageInputModalOpen
-    }));
-  }
-
-  onImageInputSubmit(val) {
-    return new Promise((resolve, reject) => {
-      this.props
-        .createMessage({
-          variables: {
-            input: {
-              authorId: localStorage.getItem("slackrUserId"),
-              channelId: this.props.channelId,
-              content: val.caption,
-              attachment: {
-                name: val.title,
-                blobFieldName: "image",
-                image: this.state.imageFile
-              }
-            }
-          }
-        })
-        .then(data => {
-          resolve(data);
-
-          this.imageInputModalToggle();
-        })
-        .catch(error => {
-          this.imageInputModalToggle();
-          reject(error);
-        });
-    });
-  }
-
-  onImageInputChange(e) {
-    const file = e.currentTarget.files[0];
-    if (!file.type.match(/image\/.*/)) {
-      throw new Error("not image error");
-    }
-
-    let reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = e => {
-      this.setState(prevState => ({
-        isImageInputModalOpen: !prevState.isImageInputModalOpen,
-        initialValues: {
-          title: file.name
-        },
-        imageFile: file,
-        imageUrl: e.target.result
-      }));
-    };
-  }
-
-  onImageAddonClick(e) {
-    this.input.click();
-  }
   render() {
     const { isSending } = this.props;
     return (
@@ -110,23 +31,7 @@ class MessageInput extends Component {
       >
         <Form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <InputGroup>
-            <InputGroupAddon onClick={this.onImageAddonClick}>
-              <span className="fa fa-file-image-o" />
-              <input
-                type="file"
-                style={{ display: "none" }}
-                ref={input => (this.input = input)}
-                onChange={this.onImageInputChange}
-              />
-              <ImageInputModal
-                isOpen={this.state.isImageInputModalOpen}
-                toggle={this.imageInputModalToggle}
-                imageUrl={this.state.imageUrl}
-                initialValues={this.state.initialValues}
-                onSubmit={this.onImageInputSubmit}
-                onCancel={this.imageInputModalToggle}
-              />
-            </InputGroupAddon>
+            <ImageInput channelId={this.props.channelId} />
             <Field
               className="form-control"
               disabled={isSending}
@@ -152,7 +57,4 @@ class MessageInput extends Component {
   }
 }
 
-export default compose(
-  reduxForm({ form: "message" }),
-  graphql(createMessageMtn, { name: "createMessage" })
-)(MessageInput);
+export default reduxForm({ form: "message" })(MessageInput);
