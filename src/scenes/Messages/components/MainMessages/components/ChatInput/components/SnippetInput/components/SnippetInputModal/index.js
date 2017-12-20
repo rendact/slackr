7993eede3675cb34db315aaa0469200ba083e7fe
ihfazhillah <mihfazhillah@gmail.com>
2017-12-20 20:40:34 +0700

    create snippetInputModal

---
+++ src/scenes/Messages/components/MainMessages/components/ChatInput/components/SnippetInput/components/SnippetInputModal/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { listLanguages } from "highlightjs";
+
+class SnippetInputModal extends Component {
+  render() {
+    const { toggle, isOpen, handleSubmit } = this.props;
+    return (
+      <Modal size="lg" toggle={toggle} isOpen={isOpen || true}>
+        <form onSubmit={handleSubmit}>
+          <ModalHeader>Create Snippet</ModalHeader>
+
+          <ModalBody>
+            <div className="form-row">
+              <div className="col-md-8">
+                <Field
+                  className="form-control"
+                  type="text"
+                  placeholder="title"
+                  component="input"
+                  name="title"
+                />
+              </div>
+              <div className="col-md-4">
+                <Field component="select" name="lang" className="form-control">
+                  <option>Auto Detect</option>
+                  {listLanguages().map((lang, id) => (
+                    <option key={id}>{lang}</option>
+                  ))}
+                </Field>
+              </div>
+              <div className="col-md-12" style={{ marginTop: 10 }}>
+                <Field
+                  component="textarea"
+                  name="snippet"
+                  className="form-control"
+                  rows="5"
+                />
+              </div>
+              <div className="col-md-12" style={{ marginTop: 10 }}>
+                <Field
+                  className="form-control"
+                  type="text"
+                  placeholder="caption"
+                  name="caption"
+                  component="input"
+                />
+              </div>
+            </div>
+          </ModalBody>
+
+          <ModalFooter>
+            <button className="btn btn-primary">Submit</button>
+            <button
+              className="btn btn-warning"
+              onClick={e => {
+                e.preventDefault();
+                toggle();
+              }}
+            >
+              Cancel
+            </button>
+          </ModalFooter>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+export default reduxForm({ form: "snippetInput" })(SnippetInputModal);
--- src/scenes/Messages/components/MainMessages/components/ChatInput/components/SnippetInput/index.js
+++ src/scenes/Messages/components/MainMessages/components/ChatInput/components/SnippetInput/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
 import { InputGroupAddon } from "reactstrap";
+import SnippetInputModal from "./components/SnippetInputModal";
 
 class SnippetInputContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.snippetInputModalToggle = this.snippetInputModalToggle.bind(this);
+
+    this.state = {
+      isSnippetInputModalOpen: false
+    };
+  }
+
+  snippetInputModalToggle() {
+    this.setState(prev => ({
+      isSnippetInputModalOpen: !prev.isSnippetInputModalOpen
+    }));
+  }
+
+  onSnippetSubmit(val) {
+    debugger;
+  }
+
   render() {
     return (
-      <InputGroupAddon style={{ cursor: "pointer" }}>
+      <InputGroupAddon
+        style={{ cursor: "pointer" }}
+        onClick={this.snippetInputModalToggle}
+      >
         <span className="fa fa-code" />
+        <SnippetInputModal
+          toggle={this.snippetInputModalToggle}
+          isOpen={this.state.isSnippetInputModalOpen}
+          onSubmit={this.onSnippetSubmit}
+        />
       </InputGroupAddon>
     );
   }
