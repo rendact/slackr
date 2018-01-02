7ad67a3d7436b1baeac43aa36814aab0111c6c2b
ihfazhillah <mihfazhillah@gmail.com>
2018-01-02 16:21:35 +0700

    add subscription to delete file/message contains file

--- src/scenes/Files/index.js
+++ src/scenes/Files/index.js
@@ -3,9 +3,35 @@ import { graphql } from "react-apollo";
 import FileItem from "./components/FileItem";
 import FilesTabContainer from "./components/FilesTabContainer";
 import allFiles from "./queries/File/allAttachments";
+import subscribeToDeleteMessage from "queries/Messages/subscribeToDelete";
 import "./files-management.css";
 
 class FilesContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.subscribeToMessageDelete = this.subscribeToMessageDelete();
+  }
+  subscribeToMessageDelete() {
+    this.props.allFiles.subscribeToMore({
+      document: subscribeToDeleteMessage,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) {
+          return prev;
+        }
+
+        const {
+          data: { subscribeToMessage: { value: { id: deletedMessageId } } }
+        } = subscriptionData;
+
+        const newEdges = prev.viewer.allMessages.edges.filter(
+          edge => edge.node.id !== deletedMessageId
+        );
+
+        return { viewer: { allMessages: { edges: newEdges } } };
+      }
+    });
+  }
   render() {
     return <FilesTabContainer allFiles={this.props.allFiles} />;
   }
